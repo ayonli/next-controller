9b96bdef2f69ce8e798b55201cc9660b08ee82dc
A-yon <i@hyurl.com>
2020-12-09 11:35:09 +0800

    useApi now supports additional options for fetch api

--- src/useApi.ts
+++ src/useApi.ts
@@ -2,27 +2,31 @@ import * as qs from "qs";
 import isEmpty from "@hyurl/utils/isEmpty";
 import HttpException from "./HttpException";
 
-export default function useApi<T>(path: string, base = ""): T {
+export default function useApi<T>(
+    path: string,
+    base = "",
+    options: Omit<RequestInit, "method" | "body"> = {}
+): T {
     const url = base + "/api/" + path;
 
     return {
-        delete: callApi.bind(void 0, "DELETE", url),
-        get: callApi.bind(void 0, "GET", url),
-        head: callApi.bind(void 0, "HEAD", url),
-        options: callApi.bind(void 0, "OPTIONS", url),
-        patch: callApi.bind(void 0, "PATCH", url),
-        post: callApi.bind(void 0, "POST", url, null),
-        put: callApi.bind(void 0, "PUT", url)
+        delete: callApi.bind(void 0, url, { ...options, method: "DELETE" }),
+        get: callApi.bind(void 0, url, { ...options, method: "GET" }),
+        head: callApi.bind(void 0, url, { ...options, method: "HEAD" }),
+        options: callApi.bind(void 0, url, { ...options, method: "OPTIONS" }),
+        patch: callApi.bind(void 0, url, { ...options, method: "PATCH" }),
+        post: callApi.bind(void 0, url, { ...options, method: "POST" }, null),
+        put: callApi.bind(void 0, url, { ...options, method: "PUT" })
     } as any;
 }
 
 async function callApi(
-    method: string,
     url: string,
+    options: Omit<RequestInit, "body"> = {},
     query: object,
-    body = void 0
+    body = void 0,
 ) {
-    const headers = {};
+    const { method, headers = {} } = options;
 
     if (!isEmpty(query)) {
         url += "?" + qs.stringify(query);
@@ -30,18 +34,18 @@ async function callApi(
 
     if (typeof body === "object") {
         if (typeof FormData !== "function" || !(body instanceof FormData)) {
-            headers["Content-Type"] = "application/json; charset=utf-8";
+            headers["Content-Type"] ||= "application/json; charset=utf-8";
             body = JSON.stringify(body);
         }
     } else if (body !== void 0) {
-        headers["Content-Type"] = "text/plain; charset=utf-8";
+        headers["Content-Type"] ||= "text/plain; charset=utf-8";
         body = String(body);
     }
 
     const trace: { stack?: string; } = {};
     Error.captureStackTrace(trace);
 
-    const res = await fetch(url, { method, headers, body });
+    const res = await fetch(url, { ...options, method, headers, body });
     let err: Error;
 
     if (res.status < 400) {
